be854575737606eaf5b000335f6211de6e022570
fatimahawny <fatimahawny@gmail.com>
2025-07-04 19:00:26 +0300

    finish weather_model

---
+++ lib/models/weather_model.dart
@@ -0,0 +1,28 @@
+class WeatherModel {
+  final String cityname;
+  final String lastupdatedtime;
+  final String? image;
+  final double temp;
+  final double maxtemp;
+  final double mintemp;
+  final String statues;
+  WeatherModel({required this.cityname,
+    required this.lastupdatedtime,
+    this.image,
+    required this.temp,
+    required this.maxtemp,
+    required this.mintemp,
+    required this.statues,}
+  );
+  factory WeatherModel.fromJson(json){
+    return WeatherModel(
+      cityname:json['location']['name'],
+      lastupdatedtime:json['current']['last_updated'],
+      temp:json['forecast']['forecastday'][0]['day']['avgtemp_c'],
+      maxtemp:json['forecast']['forecastday'][0]['day']['maxtemp_c'],
+      mintemp:json['forecast']['forecastday'][0]['day']['mintemp_c'],
+      statues:json['forecast']['forecastday'][0]['day']['condition']['text'],
+      image:json['forecast']['forecastday'][0]['day']['condition']['icon'],
+    );
+  }
+}
