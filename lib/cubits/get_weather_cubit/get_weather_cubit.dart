320561d74339268ed28bc3e91232adab2dd5dd58
fatimahawny <fatimahawny@gmail.com>
2025-08-06 20:30:01 +0300

    create get_weather_cubit.dart

---
+++ lib/cubits/get_weather_cubit/get_weather_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'get_weather_states.dart';
+
+class GetWeatherCubit extends Cubit<WeatherStates> {
+  GetWeatherCubit(super.initialState);
+
+}
